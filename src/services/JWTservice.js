87c28c100dbefe8c9410aa5f11767b7d317f709a
dennis <dennis.nguyen0909@gmail.com>
2024-12-11 08:44:16 +0700

    create-jwt-service

---
+++ src/services/JWTservice.js
@@ -0,0 +1,55 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const generalAccessToken = async (payload) => {
+  const accessToken = jwt.sign(
+    {
+      ...payload,
+    },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: "30s" }
+  );
+  return accessToken;
+};
+const generalRefreshToken = async (payload) => {
+  const refreshToken = jwt.sign(
+    {
+      ...payload,
+    },
+    process.env.REFRESH_TOKEN,
+    { expiresIn: "365d" }
+  );
+  return refreshToken;
+};
+const refreshTokenService = (token) => {
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.REFRESH_TOKEN, async (err, user) => {
+        if (err) {
+          resolve({
+            status: "Error",
+            message: "Không có quyền truy cập",
+          });
+        }
+        // Cấp lại access token mới
+        const access_token = await generalAccessToken({
+          id: user?.id,
+          isAdmin: user?.isAdmin,
+        });
+
+        resolve({
+          status: "Ok",
+          message: " Success!!",
+          access_token,
+        });
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  generalAccessToken,
+  generalRefreshToken,
+  refreshTokenService,
+};
