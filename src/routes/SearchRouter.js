19a9b295d168abe8732e63085b0776f208bdd3fd
Nguyễn Hoàng Anh Vũ <dh52112101@student.stu.edu.vn>
2024-12-11 08:28:08 +0700

    commit Router

---
+++ src/routes/SearchRouter.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const SearchController = require('../controllers/SearchController')
+
+const SearchRouter = (app) => {
+    router.post('/create', SearchController.createSearch);
+    router.get('/getAll', SearchController.getAllSearch);
+    return app.use('/api/search', router);
+}
+module.exports = SearchRouter
