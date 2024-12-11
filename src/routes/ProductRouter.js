a86f9078e9ec1e41afa55dc74a6c51e8e07af645
Nguyễn Hoàng Anh Vũ <dh52112101@student.stu.edu.vn>
2024-12-11 08:45:03 +0700

    commit Router

---
+++ src/routes/ProductRouter.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router();
+const ProductController = require('../controllers/ProductController')
+const { authMiddleware } = require('../middleware/authMiddleware')
+const ProductRouter = (app) => {
+    router.post('/create', ProductController.createProduct);
+    router.put('/update/:id', authMiddleware, ProductController.updateProduct);
+    router.delete('/delete/:id', authMiddleware, ProductController.deleteProduct);
+    // router.get('/details/:id', authMiddleware, ProductController.getDetailProduct);
+    router.get('/details/:id', ProductController.getDetailProduct);
+    router.get('/get-all-product', ProductController.getAllProduct);
+    router.get('/get-all-product2', ProductController.getAllProduct2);
+    router.post('/delete-many', authMiddleware, ProductController.deleteManyProduct);
+    router.get('/get-all-type', ProductController.getAllTypeProduct);
+    router.get('/get-all-discount', ProductController.getAllDiscount);
+
+    return app.use('/api/product', router);
+}
+
+module.exports = ProductRouter
