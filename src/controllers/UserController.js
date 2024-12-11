ed442d81ed5ce4c0baf546f78a3acd71eb12da54
dennis <dennis.nguyen0909@gmail.com>
2024-12-11 12:42:43 +0700

    fix-complete-validate

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,8 +4,14 @@ const createUser = async (req, res) => {
   try {
     //#1. Lấy ra dữ liệu & validation
     const { email, password, confirmPassword } = req.body;
+
     const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const regexPassword =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; // Password regex
+
     const isCheckEmail = regexEmail.test(email);
+    const isCheckPassword = regexPassword.test(password);
+
     if (!email || !password || !confirmPassword) {
       return res.status(200).json({
         status: "Error",
@@ -16,12 +22,19 @@ const createUser = async (req, res) => {
         status: "Error",
         message: "The email is required ....@gmail.com",
       });
+    } else if (!isCheckPassword) {
+      return res.status(200).json({
+        status: "Error",
+        message:
+          "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+      });
     } else if (password !== confirmPassword) {
       return res.status(200).json({
         status: "Error",
-        message: "Password don't match!",
+        message: "Passwords don't match!",
       });
     }
+
     //#2 truyền qua service xử lý logic
     const data = await UserService.createUser(req.body);
     return res.status(200).json({
@@ -29,7 +42,7 @@ const createUser = async (req, res) => {
     });
   } catch (error) {
     return res.status(404).json({
-      message: error,
+      message: error.message,
     });
   }
 };
